f1c8c505d7e941b649d62e2f8feb46563d96ab4a
AarohiPensalwar <102681387+AarohiPensalwar@users.noreply.github.com>
2025-12-13 15:54:36 +0530

    Add program to calculate sum using loop

---
+++ core-java/loops/SumOfNumbers.java
@@ -0,0 +1,17 @@
+package corejava.loops;
+
+/**
+ * Calculate the sum of numbers from 1 to 10
+ */
+public class SumOfNumbers {
+
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum of numbers from 1 to 10 is: " + sum);
+    }
+}
