ef4a2ae1f2c04cfc90587465d0d824f0f3f45f78
AarohiPensalwar <102681387+AarohiPensalwar@users.noreply.github.com>
2025-12-13 15:35:23 +0530

    Add welcome message program

---
+++ WelcomeMessage.java
@@ -0,0 +1,12 @@
+package corejava.basics;
+
+/**
+ * Entry program for Java QA SDET journey
+ * Author: Aarohi Pensalwar
+ */
+public class WelcomeMessage {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java QA SDET Journey - 2026");
+    }
+}
