860f1a9755ed8816907481a037f0f008a3b28f61
AarohiPensalwar <102681387+AarohiPensalwar@users.noreply.github.com>
2025-12-13 15:56:43 +0530

    Add program to reverse a number

---
+++ core-java/loops/ReverseNumber.java
@@ -0,0 +1,19 @@
+package corejava.loops;
+
+/**
+ * Reverse a given number
+ */
+public class ReverseNumber {
+
+    public static void main(String[] args) {
+        int number = 1234;
+        int reversed = 0;
+
+        while (number != 0) {
+            reversed = reversed * 10 + number % 10;
+            number = number / 10;
+        }
+
+        System.out.println("Reversed number is: " + reversed);
+    }
+}
